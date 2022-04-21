01824133d760c65d9d27d080d0fcd57a8259d805
Chathura Liyanage <84259289+CHATHURALIYANAGE1997@users.noreply.github.com>
2022-04-21 15:00:13 +0530

    Update UserRepository.java

--- src/main/java/com/example/SpringbootLoginUserRegistration/repository/UserRepository.java
+++ src/main/java/com/example/SpringbootLoginUserRegistration/repository/UserRepository.java
@@ -1,10 +1,11 @@
 package com.example.SpringbootLoginUserRegistration.repository;
 
-
+import com.example.SpringbootLoginUserRegistration.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends JpaRepository {
-
+public interface UserRepository extends JpaRepository<User,Long> {
+    User findByUsername(String username);
 }
+
