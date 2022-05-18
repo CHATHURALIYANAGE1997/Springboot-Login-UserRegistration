0c878af2c82d455611272294110e5efbd6b1edea
Chathura Liyanage <84259289+CHATHURALIYANAGE1997@users.noreply.github.com>
2022-05-18 10:59:47 +0530

    Update MyUserService.java

--- src/main/java/com/example/SpringbootLoginUserRegistration/service/MyUserService.java
+++ src/main/java/com/example/SpringbootLoginUserRegistration/service/MyUserService.java
@@ -1,16 +1,31 @@
 package com.example.SpringbootLoginUserRegistration.service;
 
+import com.example.SpringbootLoginUserRegistration.Utility.Utility;
 import com.example.SpringbootLoginUserRegistration.model.User;
 import com.example.SpringbootLoginUserRegistration.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.ui.Model;
 
+import javax.mail.MessagingException;
+import javax.servlet.http.HttpServletRequest;
+import javax.transaction.Transactional;
+import java.io.UnsupportedEncodingException;
+
+@Transactional
 @Service
 public class MyUserService implements UserDetailsService {
+    @Autowired
+    private UserRepository userRepository;
+    
     @Value("$(site.base.url.https)")
     private String emailService;
 
@@ -19,6 +34,7 @@ public class MyUserService implements UserDetailsService {
 
     @Autowired
     private JavaMailSender mailSender;
+
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
@@ -29,11 +45,11 @@ public class MyUserService implements UserDetailsService {
     }
 
     public User save(User user ){
-        user.setPassword(passwordEncoder.encode(user.getPassword()));
-       userRepository.save(user);
-        return user;
+                user.setPassword(passwordEncoder.encode(user.getPassword()));
+                userRepository.save(user);
+                return user;
     }
-    
+
     public void updateResetPasswordToken(String token, String email) throws UsernameNotFoundException {
         User user = userRepository.findByEmail(email);
         if (user != null ) {
@@ -43,11 +59,11 @@ public class MyUserService implements UserDetailsService {
             throw new UsernameNotFoundException("Could not find any customer with the email " + email);
         }
     }
-    
+
     public User getByResetPasswordToken(String token) {
         return userRepository.findByResetPasswordToken(token);
     }
-    
+
     public void updatePassword(User customer, String newPassword) {
         BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
         String encodedPassword = passwordEncoder.encode(newPassword);
@@ -56,10 +72,9 @@ public class MyUserService implements UserDetailsService {
         customer.setResetPasswordToken(null);
         userRepository.save(customer);
     }
-    
-    
+
     public void processforgotPassword(String email, String token, HttpServletRequest request, Model model){
-       try {
+                try {
             updateResetPasswordToken(token, email);
             String resetPasswordLink = Utility.getSiteURL(request) + "/reset_password?token=" + token;
             System.out.println(email);
@@ -74,21 +89,15 @@ public class MyUserService implements UserDetailsService {
         }
 
     }
+
     
-        public void sendEmail(String recipientEmail, String link)
+    public void sendEmail(String recipientEmail, String link)
             throws MessagingException, UnsupportedEncodingException {
 
 
         SimpleMailMessage message=new SimpleMailMessage();
         message.setFrom("oraclefreightsolutionspvt@gmail.com");
         message.setTo(recipientEmail);
-
-//        MimeMessage message = mailSender.createMimeMessage();
-//        MimeMessageHelper helper = new MimeMessageHelper(message);
-
-//        helper.setFrom("chathurabimalka1997@gmail.com", "Shopme Support");
-//        helper.setTo(recipientEmail);
-
         String subject = "Here's the link to reset your password";
 
         String content = "<p>Hello,</p>"
@@ -100,11 +109,7 @@ public class MyUserService implements UserDetailsService {
                 + "or you have not made the request.</p>";
         message.setSubject(subject);
         message.setText(content);
-//        helper.setSubject(subject);
-//
-//        helper.setText(content, true);
         mailSender.send(message);
     }
-
 
 }
